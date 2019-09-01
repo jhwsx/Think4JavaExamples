56e594a39abf1f1486ff39de76eaa1257bfe9db8
wangzhichao <wangzhichao@adups.com>
2019-09-01 14:52:41 +0800

    exceptions-12.12.4

---
+++ app/src/main/java/exceptions/MainException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+import java.io.FileInputStream;
+
+public class MainException {
+    public static void main(String[] args) throws Exception {
+        FileInputStream file =
+                new FileInputStream("MainException.java");
+        file.close();
+    }
+}
