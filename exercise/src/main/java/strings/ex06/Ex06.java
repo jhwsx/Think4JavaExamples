52a72ab56890a666f6f9666a75bc70db5bcb4cb1
wangzhichao <wangzhichao@adups.com>
2019-09-07 21:28:10 +0800

    strings-13.5.6

---
+++ exercise/src/main/java/strings/ex06/Ex06.java
@@ -0,0 +1,25 @@
+package strings.ex06;
+
+import android.support.annotation.NonNull;
+
+/**
+ * @author wangzhichao
+ * @date 2019/09/07
+ */
+public class Ex06 {
+    int x = 100;
+    long y = 19999999999L;
+    float f = 0.55f;
+    double d = 2.14444;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("x = %d, y = %d, f = %f, d = %f", x, y, f, d);
+    }
+
+    public static void main(String[] args) {
+        Ex06 ex06 = new Ex06();
+        System.out.println(ex06);
+    }
+}
