5c56b9c3e7b6a0e708968b25159a9891f5794524
wangzhichao <wangzhichao@adups.com>
2019-10-31 13:06:55 +0800

    strings-13.6.3 StartEnd

--- exercise/src/main/java/strings/ex13/StartEnd.java
+++ exercise/src/main/java/strings/ex13/StartEnd.java
@@ -1,4 +1,4 @@
-package strings;
+package strings.ex13;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,12 +8,15 @@ import java.util.regex.Pattern;
  * @date 2019/10/17
  */
 public class StartEnd {
-    public static String input =
-            "As long as there is injustice, whenever a\n" +
-                    "Targathian baby cries out, wherever a distress\n" +
-                    "signal sounds among the stars ... We'll be there.\n" +
-                    "This fine ship, and this fine crew ...\n" +
-                    "Never give up! Never surrender!";
+    public static final String POEM =
+            "Twas brillig, and the slithy toves\n" +
+                    "Did gyre and gimble in the wabe.\n" +
+                    "All mimsy were the borogoves,\n" +
+                    "And the mome raths outgrabe.\n\n" +
+                    "Beware the Jabberwock, my son,\n" +
+                    "The jaws that bite, the claws that catch.\n" +
+                    "Beware the Jubjub bird, and shun\n" +
+                    "The frumious Bandersnatch.";
 
     private static class Display {
         private boolean regexPrinted = false;
@@ -51,13 +54,13 @@ public class StartEnd {
     }
 
     public static void main(String[] args) {
-        for (String in : input.split("\n")) {
+        for (String in : POEM.split("\n")) {
             System.out.println("input: " + in);
             for (String regex : new String[]{
                     "\\w*ere\\w*", // \\w* 表示零个或者多个词字符
-                    "\\w*ever", //
+                    "m\\w*m\\w*", //
                     "T\\w+", // \\w+ 表示一个或者多个词字符
-                    "Never.*?!" // .*? 表示零个多个任意字符，这是勉强型，最小匹配型, !就表示感叹号字符
+                    "The.*?" // .*? 表示零个多个任意字符，这是勉强型，最小匹配型
             }) {
                 System.out.println("--------------------------examine " + regex + " start---------------------------");
                 examine(in, regex);
