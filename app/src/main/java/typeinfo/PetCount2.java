fa1a1544d7321e0d6d57166428eefe0a98b631f2
wangzhichao <wangzhichao@adups.com>
2019-11-30 17:18:47 +0800

    14.3.1

---
+++ app/src/main/java/typeinfo/PetCount2.java
@@ -0,0 +1,13 @@
+package typeinfo;
+
+import typeinfo.pets.Pets;
+
+/**
+ * @author wangzhichao
+ * @date 2019/11/30
+ */
+public class PetCount2 {
+    public static void main(String[] args) {
+        PetCount.countPets(Pets.creator);
+    }
+}
--- app/src/main/java/typeinfo/pets/Pets.java
+++ app/src/main/java/typeinfo/pets/Pets.java
@@ -1,18 +1,25 @@
 //: typeinfo/pets/Pets.java
 // Facade to produce a default PetCreator.
 package typeinfo.pets;
+
 import java.util.*;
 
+/**
+ * 这里是创建一个使用了 LiteralPetCreator 的外观
+ */
 public class Pets {
-  public static final PetCreator creator =
-    new LiteralPetCreator();
-  public static Pet randomPet() {
-    return creator.randomPet();
-  }
-  public static Pet[] createArray(int size) {
-    return creator.createArray(size);
-  }
-  public static ArrayList<Pet> arrayList(int size) {
-    return creator.arrayList(size);
-  }
+    public static final PetCreator creator =
+            new LiteralPetCreator();
+
+    public static Pet randomPet() {
+        return creator.randomPet();
+    }
+
+    public static Pet[] createArray(int size) {
+        return creator.createArray(size);
+    }
+
+    public static ArrayList<Pet> arrayList(int size) {
+        return creator.arrayList(size);
+    }
 } ///:~
