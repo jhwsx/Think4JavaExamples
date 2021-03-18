627bee2fb43c87efb9d7ec27eca0c445ba712c76
wangzhichao <willwaywang6@gmail.com>
2021-03-18 09:50:55 +0800

    review concurrency

--- codes/src/concurrency/ExplicitCriticalSection.java
+++ codes/src/concurrency/ExplicitCriticalSection.java
@@ -6,11 +6,14 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * 使用 Lock 对象来创建明显的临界区
  * 这里只是演示代码，跑起来会有问题。主要用来说明 Lock 对象也可以创建明显的临界区。
+ * 这个例子修复了，请看 {@link concurrency.exlicitcriticalsectionfix.ExplicitCriticalSectionFix} 这个类的代码。
+ *
  * @author wangzhichao
  * @since 2020/3/13
  */
 class ExplicitPairManager1 extends PairManager {
     private Lock lock = new ReentrantLock();
+
     @Override
     public void increment() {
         lock.lock();
@@ -26,6 +29,7 @@ class ExplicitPairManager1 extends PairManager {
 
 class ExplicitPairManager2 extends PairManager {
     private Lock lock = new ReentrantLock();
+
     @Override
     public void increment() {
         Pair temp;
@@ -40,6 +44,7 @@ class ExplicitPairManager2 extends PairManager {
         store(temp);
     }
 }
+
 public class ExplicitCriticalSection {
     public static void main(String[] args) {
         ExplicitPairManager1 explicitPairManager1 = new ExplicitPairManager1();
